69aa38d77efc56241d835fa15ae8d23b52928b74
Mark Ivanov <mark@MacAir.local>
2025-07-10 11:59:11 +0300

    Init commit

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+// src/main.jsx
+import React, { useMemo } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+const Main = () => {
+    const network = 'devnet';
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+    return (
+        <React.StrictMode>
+            <ConnectionProvider endpoint={endpoint}>
+                <WalletProvider wallets={wallets} autoConnect>
+                    <WalletModalProvider>
+                        <App />
+                    </WalletModalProvider>
+                </WalletProvider>
+            </ConnectionProvider>
+        </React.StrictMode>
+    );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Main />);
